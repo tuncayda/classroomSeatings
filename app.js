8d178e880bb14cdd88cd912ba0df6eff9ca43e99
tuncayda <tuncaydagdelen89@gmail.com>
2022-05-14 15:31:31 +0200

    added css assets

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-const express = require('express');
 require('dotenv').config();
-const app = express();
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
 
+const app = express();
 const hostname = process.env.HOST;
 const port = process.env.PORT;
 
+app.use(cors());
+
+// Serve assets
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(port, () => {
