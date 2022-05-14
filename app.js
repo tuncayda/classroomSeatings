0860ead9247c3c7af22fc0dd19adf5a4d44480bf
tuncayda <tuncaydagdelen89@gmail.com>
2022-05-14 12:24:34 +0200

    added dotenv file

---
+++ app.js
@@ -0,0 +1,15 @@
+const http = require('http');
+require('dotenv').config()
+
+const hostname = process.env.HOST;
+const port = process.env.PORT;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
