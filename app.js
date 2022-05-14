f0af050c39bbeb4584c74bd72105d6a91522a8a3
tuncayda <tuncaydagdelen89@gmail.com>
2022-05-14 16:02:32 +0200

    pug templating engine

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const pug = require('pug');
 
 const app = express();
 const hostname = process.env.HOST;
@@ -12,8 +13,12 @@ app.use(cors());
 // Serve assets
 app.use(express.static(__dirname + '/public'));
 
+// Set pug templating engine
+app.set('views', './views');
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
+	res.render('index');
 });
 
 app.listen(port, () => {
