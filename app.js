e93ea9dbe86448270e050eaba76dcb5af3c9bfca
tuncayda <tuncaydagdelen89@gmail.com>
2022-05-15 20:40:38 +0200

    prepared endpoint for loading classes

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const pug = require('pug');
+const { nextTick } = require('process');
+const { render } = require('express/lib/response');
 
 const app = express();
 const hostname = process.env.HOST;
@@ -15,16 +17,25 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/node_modules/gridstack'));
 
 // Set pug templating engine
-// app.set('views', './views');
-// app.set('view engine', 'pug');
-
-// app.get('/', (req, res) => {
-// 	res.render('index');
-// });
+app.set('views', './views');
+app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
+	res.render('index');
 });
+
+// TODO: använda denna endpoint för att visa specifik klass
+app.get('/class/:name', (req, res) => {
+    const className = req.params.name;
+    // TODO: hämta klassen, skicka den till "class" och rendera klassen där
+    // Koden nedan är inte helt korrekt. Bättre att skicka med en lista med elevernas namn istället för "classname"
+    // Däremot kan "classname" användas för att hämta alla elevers namn i den klassen + eventuella inställningar för klassen
+    res.render('class', className);
+});
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname + '/index.html'));
+// });
 
 app.listen(port, () => {
     console.log(`Running on port ${port}`);
