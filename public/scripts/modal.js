a234b984d1a8f58f164fbc643176e93aa2e7e153
tuncayda <tuncaydagdelen89@gmail.com>
2022-06-26 00:56:08 +0200

    added aos animation

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,73 +1,73 @@
 import { SchoolClass, addToClass, addClassroom } from './schoolClass.js';
 
 function updateSchoolClasses() {
-    let db = JSON.parse(localStorage.getItem('db'));
-    let schoolClass = db[db.length - 1];
-    const parent = document.querySelector('.modal-content-top');
+  let db = JSON.parse(localStorage.getItem('db'));
+  let schoolClass = db[db.length - 1];
+  const parent = document.querySelector('.modal-content-top');
 
-    let button = document.createElement('button');
-    button.innerHTML = schoolClass.name;
-    button.setAttribute('data-schoolclass-id', schoolClass.id);
-    button.classList.add('modal-content-schoolClass');
-    parent.append(button);
+  let button = document.createElement('button');
+  button.innerHTML = schoolClass.name;
+  button.setAttribute('data-schoolclass-id', schoolClass.id);
+  button.classList.add('modal-content-schoolClass');
+  parent.append(button);
 }
 
 function populateModalContent() {
-    let db = JSON.parse(localStorage.getItem('db'));
-    const parent = document.querySelector('.modal-content-top');
+  let db = JSON.parse(localStorage.getItem('db'));
+  const parent = document.querySelector('.modal-content-top');
 
-    for (let i = 0; i < db.length; i++) {
-        let button = document.createElement('button');
-        button.innerHTML = db[i].name;
-        button.setAttribute('data-schoolClass-id', db[i].id);
-        button.classList.add('modal-content-schoolClass');
-        parent.append(button);
-    }
+  for (let i = 0; i < db.length; i++) {
+    let button = document.createElement('button');
+    button.innerHTML = db[i].name;
+    button.setAttribute('data-schoolClass-id', db[i].id);
+    button.classList.add('modal-content-schoolClass');
+    parent.append(button);
+  }
 }
 
 function clearModalContent() {
-    const parent = document.querySelector('.modal-content-top');
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-    }
+  const parent = document.querySelector('.modal-content-top');
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
 }
 
 function addModalButtons() {
-    // Adding class FE
-   let addClassButton = document.querySelector('.modal-content-addClassButton');
-   addClassButton.addEventListener('click', () => {
-     
-     // Show input field and action buttons
-     let inputField = document.querySelector('.modal-content-actions');
-     addClassButton.style.display = 'none';
-     inputField.style.display = 'block';
-   
-     // Clear input field
-     let input = document.querySelector('.modal-content-actions-inputSchoolClassName');
-     input.addEventListener('focusin', () => {
-       input.value = '';
-     });
-   
-     // Cancel
-     document.querySelector('.modal-content-actions-cancel').addEventListener('click', () => {
-       addClassButton.style.display = 'block';
-       input.value = 'Klassnamn';
-       inputField.style.display = 'none';
-     });
-   });
-   
-   // Submit schoolClass
-   let submitButton = document.querySelector('.modal-content-actions-submit');
-   submitButton.addEventListener('click', () => {
-     let schooClassName = document.querySelector('.modal-content-actions-inputSchoolClassName').value;
-     if(schooClassName.length < 1 || schooClassName === 'Klassnamn') {
-       alert('Class name must be longer than 1 character');
-     } else {
-       addClassroom(schooClassName.toLocaleUpperCase());
-       updateSchoolClasses();
-     }
-   });
-  }
+  // Adding class FE
+  let addClassButton = document.querySelector('.modal-content-addClassButton');
+  addClassButton.addEventListener('click', () => {
+
+    // Show input field and action buttons
+    let inputField = document.querySelector('.modal-content-actions');
+    addClassButton.style.display = 'none';
+    inputField.style.display = 'block';
+
+    // Clear input field
+    let input = document.querySelector('.modal-content-actions-inputSchoolClassName');
+    input.addEventListener('focusin', () => {
+      input.value = '';
+    });
+
+    // Cancel
+    document.querySelector('.modal-content-actions-cancel').addEventListener('click', () => {
+      addClassButton.style.display = 'block';
+      input.value = 'Klassnamn';
+      inputField.style.display = 'none';
+    });
+  });
+
+  // Submit schoolClass
+  let submitButton = document.querySelector('.modal-content-actions-submit');
+  submitButton.addEventListener('click', () => {
+    let schooClassName = document.querySelector('.modal-content-actions-inputSchoolClassName').value;
+    if (schooClassName.length < 1 || schooClassName === 'Klassnamn') {
+      alert('Class name must be longer than 1 character');
+    } else {
+      addClassroom(schooClassName.toLocaleUpperCase());
+      updateSchoolClasses();
+    }
+  });
+}
 
 /**
  * Add student
@@ -88,17 +88,23 @@ addStudentButton.addEventListener('click', () => {
   // Submit button
   let submitStudent = document.querySelector('.modal-content-schoolClass-students-actions-submit');
   submitStudent.addEventListener('click', () => {
-  let firstname = document.querySelector('.inputFirstname').value;
-  let lastname = document.querySelector('.inputLastname').value;
-  let schoolClassID = 1656159433571;
-  // let schoolClassID = submitStudent.closest('.modal-content-schoolClass').getAttribute('data-schoolclass-id');
+    let firstname = document.querySelector('.inputFirstname').value;
+    let lastname = document.querySelector('.inputLastname').value;
 
-  
-
-  addToClass(firstname, lastname, schoolClassID);
+    if (firstname.length < 1 || firstname == 'Förnamn'|| lastname.length < 1 || lastname == 'Efternamn') {
+      alert('Please enter a name');
+    } else {
+      let schoolClassID = 1656159433571;
+      // let schoolClassID = submitStudent.closest('.modal-content-schoolClass').getAttribute('data-schoolclass-id');
+      addToClass(firstname, lastname, schoolClassID);
+    }
+  });
 });
-  
+
+
+
+AOS.init({
+  duration: 550,
+  easing: 'ease-out-back'
 });
-
-
 export { updateSchoolClasses, populateModalContent, clearModalContent, addModalButtons }
